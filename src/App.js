bbc7cb2a86883c34e75dc0b146fa6e0ae9aaa65f
shubhamsingh83 <akashsingh832001@gmail.com>
2024-03-28 10:06:30 +0530

    states1

--- src/App.js
+++ src/App.js
@@ -113,7 +113,7 @@ function App() {
      </div>
      {selectedCity && (
         <h2 className="result">
-          You selected <span className="highlight">{selectedCity},</span>
+          You selected <span className="highlight">{selectedCity}</span>
           <span className="fade">
             {" "}
             {selectedState},{selectedCountry}
